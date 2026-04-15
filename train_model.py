1c3e453ef9deec8ece7b4b94b4165c627d094a90
Seelam Deepika <149002267+9059963940@users.noreply.github.com>
2026-04-15 15:33:21 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -4,11 +4,14 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 import joblib
 
+# Load dataset
 data = pd.read_csv("predictive_maintenance.csv")
 
+# Clean
 data = data.drop(['UDI', 'Product ID'], axis=1, errors='ignore')
 data.columns = data.columns.str.strip()
 
+# Target column
 target_col = "Machine failure" if "Machine failure" in data.columns else data.columns[-1]
 
 X = data.drop(target_col, axis=1)
@@ -16,16 +19,28 @@ y = data[target_col]
 
 X = pd.get_dummies(X, drop_first=True)
 
+# Split
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
+# Model
 model = RandomForestClassifier(
-    n_estimators=300,
-    max_depth=12,
+    n_estimators=200,
+    max_depth=10,
     class_weight="balanced",
     random_state=42
 )
+
 model.fit(X_train, y_train)
 
+# Accuracy
+y_pred = model.predict(X_test)
+acc = accuracy_score(y_test, y_pred)
+
+print("Model Accuracy:", acc)
+
+# Save model
+joblib.dump(model, "model.pkl")
+joblib.dump(X.columns, "features.pkl")
 
